60ef51be476d4e66f24f9df43aa0a5ca40f553ff
BuiVoQuangVinh <buivoquangvinh9@gmail.com>
2022-08-03 19:41:42 +0700

    387. First Unique Character in a String

--- main.js
+++ main.js
@@ -1,33 +1,23 @@
-const nums = [18, 43, 36, 13, 7]
+// 387. First Unique Character in a String
+const s = "lovel eetco de"
 
-var sumOfDigits = (num) => {
-    let total = 0
-    while (num > 0) {
-        const temp = num % 10;
-        num = Math.floor(num / 10)
-        total += temp
-    }
-    return total
-}
-
-var maximumSum = function (nums) {
-    let temp = {}
-    let result = -1;
-    for (let i = 0; i < nums.length; i++) {
-        const sumOfDigitsI = sumOfDigits(nums[i])
-        if (temp[sumOfDigitsI]) {
-            if (result < nums[i] + temp[sumOfDigitsI]) {
-                result = nums[i] + temp[sumOfDigitsI]
-            }
-            if (nums[i] > temp[sumOfDigitsI]) {
-                temp = { ...temp, [sumOfDigitsI]: nums[i] }
-            }
+var firstUniqChar = function (s) {
+    const arrFromS = Array.from(s)
+    const hashTable = arrFromS.reduce((obj, currentValue) => {
+        if (obj[currentValue]) {
+            return { ...obj, [currentValue]: obj[currentValue] + 1 }
         }
         else {
-            temp = { ...temp, [sumOfDigitsI]: nums[i] }
+
+            return { ...obj, [currentValue]: 1 }
+        }
+    }, {})
+    for (let i = 0; i < arrFromS.length; i++) {
+        if (hashTable[arrFromS[i]] === 1) {
+            return i
         }
     }
-    return result
-}
+    return -1
+};
 
-console.log(maximumSum(nums))
+console.log(firstUniqChar(s))
