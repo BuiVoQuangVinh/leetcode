f02e201a6658f770d611ab620adf934540514f82
BuiVoQuangVinh <buivoquangvinh9@gmail.com>
2022-08-21 22:35:43 +0700

    53. Maximum Subarray

--- main.js
+++ main.js
@@ -1,31 +1,23 @@
-// 165. Compare Version Numbers
+// 53. Maximum Subarray
 
 
-version1 = "1.0.1", version2 = "1"
 
-var compareVersion = function (version1, version2) {
-    const arr1 = Array.from(version1.split('.'), Number)
-    const arr2 = Array.from(version2.split('.'), Number)
-    let i = 0
-    console.log({arr1,arr2})
-    while (arr1[i]>=0 || arr2[i]>=0) {
-        if (arr1[i] < arr2[i]) {
-            return -1
-        } else if (arr1[i] > arr2[i]) {
-            return 1
-        } else if (arr1[i] === undefined) {
-            if (arr2[i] !== 0) {
-                return -1
-            }
-        } else if (arr2[i] === undefined) {
-            if (arr1[i] !== 0) {
-                return 1
-            }
+nums = [5,4,-1,7,8]
+
+var maxSubArray = function (nums) {
+    let result = nums[0]
+    let temp =[...nums]
+    for (let i = 1; i < temp.length; i++) {
+        if(temp[i]+temp[i-1] >temp[i]){
+            temp[i] = temp[i]+temp[i-1]
         }
-        i++
-        console.log({i})
+        if(temp[i]>result){
+            result = temp[i]
+        }
     }
-    return 0
+
+    return result
 };
 
-console.log(compareVersion(version1,version2))
+console.log(maxSubArray(nums))
+console.log(nums)
