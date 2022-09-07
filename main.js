2552f61b5bfc0a7caeb99a250cec795bc871708d
BuiVoQuangVinh <buivoquangvinh9@gmail.com>
2022-09-08 02:28:20 +0700

    leetcode 12.IntegertoRoman

--- main.js
+++ main.js
@@ -1,29 +1,107 @@
-// 11. Container With Most Water
+// 12. Integer to Roman
 
-height = [1,8,6,2,5,4,8,3,7]
+num = 1994
 
-
-var maxArea = function(height) {
-    let result = -1
-    let right = height.length -1 
-    let left = 0
-
-    while (left< right){
-        const current = (right-left)*Math.min(height[right],height[left])
-
-        if (result < current){
-            result = current
+var intToRoman = function (num) {
+    // const hashTable = {
+    //     1: 'I',
+    //     4: 'IV',
+    //     5: 'V',
+    //     9: 'IX',
+    //     10: 'X',
+    //     40: 'XL',
+    //     50: 'L',
+    //     90: 'XC',
+    //     100: 'C',
+    //     400: 'CD',
+    //     500: 'D',
+    //     900: 'CM',
+    //     1000: 'M',
+    // }
+    let result = ''
+    while (num) {
+        if (num >= 1000) {
+            const step = Math.floor(num / 1000)
+            for (let i = 0; i < step; i++) {
+                result += 'M'
+                num-=1000
+            }
         }
 
-        if(height[left] < height[right]){
-            left ++
+        else if (num >= 100) {
+            if (num >= 900) {
+                result += 'CM'
+                num -= 900
+               
+            }
+            else if (num >= 500) {
+                result += 'D'
+                num -= 500
+            }
+
+            else if (num >= 400) {
+                result += 'CD'
+                num -= 400
+            }
+            const step = Math.floor(num / 100)
+            for (let i = 0; i < step; i++) {
+                result += 'C'
+                num-=100
+            }
+
         }
-        else{
-            right --
+
+        else if (num >= 10) {
+            if (num >= 90) {
+                result += 'XC'
+                num -= 90
+            }
+
+            else if (num >= 50) {
+                result += 'L'
+                num -= 50
+            }
+
+            else if (num >= 40) {
+                result += 'XL'
+                num -= 40
+            }
+
+            const step = Math.floor(num / 10)
+            for (let i = 0; i < step; i++) {
+                result += 'X'
+                num-=10
+            }
+
+        }
+
+        else  {
+            if (num >= 9) {
+                result += 'IX'
+                num -= 9
+            }
+
+            else if (num >= 5) {
+                result += 'V'
+                num -= 5
+            }
+
+            else if (num >= 4) {
+                result += 'IV'
+                num -= 4
+            }
+
+            const step = Math.floor(num / 1)
+            for (let i = 0; i < step; i++) {
+                result += 'I'
+                num-=1
+            }
+
         }
     }
-    return result
 
+    return result
 };
 
-console.log(maxArea(height))
+console.log(intToRoman(num))
+
