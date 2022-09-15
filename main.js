47edf4957963d79ffdbb440a5f626c5f1728dd25
BuiVoQuangVinh <buivoquangvinh9@gmail.com>
2022-09-15 16:10:47 +0700

    leetcode 3 sum

--- main.js
+++ main.js
@@ -1,26 +1,41 @@
-const n = 10
-const minNumber = (number)=>{
-    if ( number <10){
-        return number
-    }
+// 15. 3Sum
 
-    if (number > 45){
-        return -1
-    }
-    let result = 0;
-    let temp = 1;
-    let count = 9
-    while (number>0){
-        result+= temp*count
-        temp*=10
-        number -= count
-        count --
-        if(number <= count){
-            result+= temp*number
-            return result
+nums = [-2,0,1,1,2]
+
+var threeSum = function(nums) {
+        const newNums = nums.sort( (a,b)=>a-b  )
+        const result = []
+        for(let i = 0;i < newNums.length -2;i++){
+            let left  = i+1;
+            let right = newNums.length -1;
+            while( left < right){
+                if( newNums[left] + newNums[right]+newNums[i] === 0){
+                    let isValid = true
+                    for (let arr of result ){
+                        if ( arr[0]=== newNums[i] && arr[1]=== newNums[left]&&arr[2]===newNums[right]){
+                            isValid = false
+                        }
+
+                    }
+                    if(isValid){
+                        result.push( [  newNums[i],newNums[left],newNums[right]])
+                        right --
+                    }
+                    else{
+                        right --
+                    }
+                }
+
+                else if( newNums[left] + newNums[right]+newNums[i] > 0){
+                    right --
+                }
+                else {
+                    left++
+                }
+            }
         }
-    }
-    return result
-}
+        return result
+};
 
-console.log(minNumber(n))
+console.log(threeSum(nums))
+
