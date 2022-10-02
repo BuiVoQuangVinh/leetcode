671051db4b0c63841c41be868d7d08af3c56d2fe
BuiVoQuangVinh <buivoquangvinh9@gmail.com>
2022-10-03 02:50:52 +0700

    leetcode 1816.TruncateSentence

--- main.js
+++ main.js
@@ -1,23 +1,14 @@
-// 1404. Number of Steps to Reduce a Number in Binary Representation to One
-s = '1111011110000011100000110001011011110010111001010111110001'
+// 1816. Truncate Sentence
+s = "chopper is not a tanuki", k = 5
 
 
-var numSteps = function (s) {
-
-    let num = BigInt(`0b${s}`)
-
-    let steps = 0
-    while (num != 1n) {
-        if (num % 2n === 1n) {
-            num += 1n
-            steps += 1
-        }
-        else {
-            num = num / 2n
-            steps += 1
-        }
+var truncateSentence = function(s, k) {
+    const arr = s.split(" ")
+    let result = arr[0]
+    for(let i =1;i<k;i++){
+        result +=" "+arr[i]
     }
-    return steps
-};
+    return result
+};  
 
-console.log(numSteps(s))
+console.log(truncateSentence(s,k))
