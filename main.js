485ed6ce5eae0b3cacff4a2bd44c109f66e0bac0
BuiVoQuangVinh <buivoquangvinh9@gmail.com>
2023-05-07 00:29:44 +0700

    1217.MinimumCosttoMoveChipstoTheSamePosition

--- main.js
+++ main.js
@@ -1,46 +1,19 @@
-// 2299. Strong Password Checker II
+// 1217. Minimum Cost to Move Chips to The Same Position
 
-password = "AaBbCc!"
 
-var strongPasswordCheckerII = function(password) {
-    let leastLetter = 8
 
-    let lowercase = false
-    let uppercase = false
-    let digit = false
-    let specialCharacter = false
-    let sameCharinAdjacent = true
-    
-    if(password.leng < leastLetter){
-        return false
+var minCostToMoveChips = function (position) {
+    let even = 0;
+    let odd = 0
+    for (let i = 0; i < position.length; i++) {
+        if (position[i] % 2 === 0) {
+            even++
+        }
+        else {
+            odd++
+        }
     }
 
+    return even < odd ? even : odd
 
-
-    for (let i=0;i<password.length;i++){
-
-        if( password[i] ===  password[i+1]){
-            return false
-        }
-        // 65:A -> 90:Z
-        if( password[i].charCodeAt()>=65 && password[i].charCodeAt()<=90 ){
-            uppercase = true
-        }
-        // 97:a ->122:z
-        else if( password[i].charCodeAt()>=97 && password[i].charCodeAt()<=122 ){
-            lowercase = true
-        }
-
-        else if( password[i].charCodeAt()>=48 && password[i].charCodeAt()<=57 ){
-            digit = true
-        }
-        else{
-            specialCharacter = true
-        }
-       
-
-    }
-
-    return lowercase&& uppercase&&digit&&specialCharacter&&sameCharinAdjacent
 };
-console.log(strongPasswordCheckerII(password))
