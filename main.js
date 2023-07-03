4e5c5ac93a0201206878c20dc765627fd03b5316
BuiVoQuangVinh <buivoquangvinh9@gmail.com>
2023-07-04 02:20:47 +0700

    2220.MinimumBitFlipstoConvertNumber

--- main.js
+++ main.js
@@ -1,33 +1,14 @@
-const nums = [18, 43, 36, 13, 7]
-
-var sumOfDigits = (num) => {
-    let total = 0
-    while (num > 0) {
-        const temp = num % 10;
-        num = Math.floor(num / 10)
-        total += temp
-    }
-    return total
-}
-
-var maximumSum = function (nums) {
-    let temp = {}
-    let result = -1;
-    for (let i = 0; i < nums.length; i++) {
-        const sumOfDigitsI = sumOfDigits(nums[i])
-        if (temp[sumOfDigitsI]) {
-            if (result < nums[i] + temp[sumOfDigitsI]) {
-                result = nums[i] + temp[sumOfDigitsI]
-            }
-            if (nums[i] > temp[sumOfDigitsI]) {
-                temp = { ...temp, [sumOfDigitsI]: nums[i] }
-            }
-        }
-        else {
-            temp = { ...temp, [sumOfDigitsI]: nums[i] }
+var minBitFlips = function (start, goal) {
+    let result = 0
+    // xor tim cac binary khac nhau cua tung so => so buoc can doi
+    let stringXor = (start ^ goal).toString(2)
+    for (let i = 0; i < stringXor.length; i++){
+        if(stringXor[i] == 1){
+            result+=1
         }
     }
-    return result
-}
+    return result;
+};
 
-console.log(maximumSum(nums))
+
+console.log(minBitFlips(5, 3));
