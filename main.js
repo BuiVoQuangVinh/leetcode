3e16e644ae934db62ea9621442c26497c5e9fede
BuiVoQuangVinh <buivoquangvinh9@gmail.com>
2023-07-05 00:24:43 +0700

    2224.MinimumNumberofOperationstoConvertTime

--- main.js
+++ main.js
@@ -1,14 +1,44 @@
-var minBitFlips = function (start, goal) {
+// 2224. Minimum Number of Operations to Convert Time
+
+
+var convertTime = function (current, correct) {
     let result = 0
-    // xor tim cac binary khac nhau cua tung so => so buoc can doi
-    let stringXor = (start ^ goal).toString(2)
-    for (let i = 0; i < stringXor.length; i++){
-        if(stringXor[i] == 1){
-            result+=1
-        }
+    let hours = 0
+    let mins = correct.slice(3, 5) - current.slice(3, 5)
+    if(mins<0){
+        mins+=60
+        hours = correct.slice(0, 2) - current.slice(0, 2) - 1
     }
-    return result;
+    else{
+
+        hours = correct.slice(0, 2) - current.slice(0, 2)
+    }
+
+    if (hours > 0) {
+        let temp = hours
+        hours -= temp
+        result += temp
+    }
+
+    if (mins >= 15) {
+        let temp = parseInt(mins / 15)
+        mins -= temp * 15
+        result += temp
+    }
+
+    if (mins >= 5) {
+        let temp = parseInt(mins / 5)
+        mins -= temp * 5
+        result += temp
+    }
+    if (mins > 0) {
+        let temp = mins
+        mins -= temp
+        result += temp
+    }
+
+    return result
 };
 
 
-console.log(minBitFlips(5, 3));
+console.log(convertTime("00:00", "23:59"));
