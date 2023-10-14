e12818b440d4fa43ef02829e011d4e12a04d5f99
BuiVoQuangVinh <buivoquangvinh9@gmail.com>
2023-10-15 02:11:17 +0700

    1652.DefusetheBomb

--- main.js
+++ main.js
@@ -1,27 +1,44 @@
 // 492. Construct the Rectangle
 
-gain = [-5,1,5,0,-7]
+code = [2, 4, 9, 3], k = -2
 
-var largestAltitude = function (gain) {
-    // let current = 0
-    // let highest = 0
+var decrypt = function (code, k) {
+    if (k === 0) {
 
-    // for (let i = 0; i < gain.length; i++) {
-    //     current += gain[i]
-    //     if (highest < current) {
-    //         highest = current
-    //     }
-
-    // }
-    // return highest
-
-    let arr = []
-    arr[0] = 0
-    for(let i=0;i<gain.length;i++){
-        arr[i+1] = arr[i]+ gain[i]
+        return new Array(code.length).fill(0)
     }
-    return Math.max(...arr)
+    let result = []
+    const length = code.length
+    if (k > 0) {
+        for (let i = 0; i < length; i++) {
+            let temp = 0
+
+            for (let j = 1; j <= k; j++) {
+                temp += code[(i + j) % length]
+            }
+            result[i] = temp
+        }
+        return result
+    }
+
+    else {
+        for (let i = 0; i < length; i++) {
+            let temp = 0
+            for (let j = -1; j >= k; j--) {
+                if (i + j >= 0) {
+                    temp += code[i + j]
+                }
+                else {
+                    temp += code[length + i + j]
+                }
+
+            }
+            result[i] = temp
+        }
+        return result
+    }
+
 
 };
 
-console.log(largestAltitude(gain));
+console.log(decrypt(code, k));
