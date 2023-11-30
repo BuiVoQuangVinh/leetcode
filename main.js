4093ee51b3a556f2a1fda8bf43a52e0641ed8e95
BuiVoQuangVinh <buivoquangvinh9@gmail.com>
2023-11-30 10:59:05 +0700

    1668.MaximumRepeatingSubstring

--- main.js
+++ main.js
@@ -1,74 +1,44 @@
-// // 448. Find All Numbers Disappeared in an Array
+// 1668. Maximum Repeating Substring
+
+sequence = "aaabaaaabaaabaaaabaaaabaaaabaaaaba", word = "aaaba"
+//  = "aaabaaaab aaaba aaaba aaaba aaaba aaaba"
 
 
-
-
-// 2384. Largest Palindromic Number
-// num = '4449471379'
-num = '6006'
-
-
-var largestPalindromic = function (num) {
-    if(parseInt(num) ===0){
-        return '0'
-    }
-
-    const obj = {}
-    even = []
-    odd = -1
-    for (let i = 0; i < num.length; i++) {
-        if (!obj[num[i]]) {
-            obj[num[i]] = 1
-        }
-        else {
-            obj[num[i]] += 1
-        }
-    }
-
-
-    for (let key in obj) {
-        if (obj[key] % 2 === 0) {
-            while (obj[key] > 0) {
-                even.push(parseInt(key))
-                obj[key] -= 2
-            }
-        }
-        else {
-            if (odd < parseInt(key)) {
-                odd = parseInt(key)
-            }
-            if (obj[key] > 1) {
-                while (obj[key] > 1) {
-                    even.push(parseInt(key))
-                    obj[key] -= 2
+var maxRepeating = function (sequence, word) {
+    let result = 0
+    let temp = 0
+    for (let i = 0; i < sequence.length; i++) {
+        console.log({ result,i })
+        if (sequence[i] === word[0]) {
+            for (let j = 0; j < word.length; j++) {
+                if (sequence[i + j] !== word[j]) {
+                    if (temp > result) {
+                        result = temp
+                    }
+                    i -= word.length * temp
+                    temp = 0
+                    break;
+                }
+                else {
+                    if (j === word.length - 1) {
+                        temp++
+                        i += j
+                    }
                 }
             }
-     
-
         }
+        else {
+            if (temp > result) {
+                result = temp
+            }
+            i -= word.length * temp
+            temp = 0
+        }
+
+
     }
-
-
-    even.sort((a, b) => b - a)
-    if(even[0] === even[even.length -1] && even[0] ===0 ){
-        return`${odd}`
-    }
-
-
-    let result = '';
-    for (let i = 0; i < even.length; i++) {
-        result += `${even[i]}`
-    }
-
-    if(odd >=0){
-        result += `${odd}`
-    }
-
-    for (let i = even.length - 1; i >= 0; i--) {
-        result += `${even[i]}`
-    }
-
-    return result
+    return result > temp ? result : temp;
 };
 
-console.log(largestPalindromic(num));
+
+console.log(maxRepeating(sequence, word))
