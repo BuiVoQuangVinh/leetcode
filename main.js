0460e18cdf975f82063c65ee413551f8d3f54d2f
BuiVoQuangVinh <buivoquangvinh9@gmail.com>
2023-11-30 13:41:52 +0700

    383.RansomNote

--- main.js
+++ main.js
@@ -1,44 +1,38 @@
-// 1668. Maximum Repeating Substring
-
-sequence = "aaabaaaabaaabaaaabaaaabaaaabaaaaba", word = "aaaba"
-//  = "aaabaaaab aaaba aaaba aaaba aaaba aaaba"
+// 383. Ransom Note
+ransomNote = "abc", magazine = "b"
 
 
-var maxRepeating = function (sequence, word) {
-    let result = 0
-    let temp = 0
-    for (let i = 0; i < sequence.length; i++) {
-        console.log({ result,i })
-        if (sequence[i] === word[0]) {
-            for (let j = 0; j < word.length; j++) {
-                if (sequence[i + j] !== word[j]) {
-                    if (temp > result) {
-                        result = temp
-                    }
-                    i -= word.length * temp
-                    temp = 0
-                    break;
-                }
-                else {
-                    if (j === word.length - 1) {
-                        temp++
-                        i += j
-                    }
-                }
+var canConstruct = function (ransomNote, magazine) {
+    const countString = (str) => {
+        const obj = {}
+        for (let i = 0; i < str.length; i++) {
+            if (!obj[str[i]]) {
+                obj[str[i]] = 1
+            }
+            else {
+                obj[str[i]] += 1
             }
         }
-        else {
-            if (temp > result) {
-                result = temp
-            }
-            i -= word.length * temp
-            temp = 0
-        }
-
-
+        return obj
     }
-    return result > temp ? result : temp;
+    const objRansomNot = countString(ransomNote)
+    const objMagazine = countString(magazine)
+
+    for (let key in objRansomNot) {
+        if (objRansomNot[key] > objMagazine[key]) {
+            return false
+        }
+        else if (objRansomNot[key]) {
+            if (!objMagazine[key]){
+                return false
+            }
+        }
+    }
+    return true
+
+
+
 };
 
+console.log(canConstruct(ransomNote, magazine))
 
-console.log(maxRepeating(sequence, word))
