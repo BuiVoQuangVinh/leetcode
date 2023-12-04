131ad997eadb5f51f66ce798335020870215303e
BuiVoQuangVinh <buivoquangvinh9@gmail.com>
2023-12-04 13:49:52 +0700

    1005. Maximize Sum Of Array After K Negations

--- main.js
+++ main.js
@@ -1,37 +1,72 @@
-// 976. Largest Perimeter Triangle
+// 1005. Maximize Sum Of Array After K Negations
 
-nums = [3,2,3,4]
+nums = [-4, -2, -3], k = 4
 
-var largestPerimeter = function (nums) {
-    let perimeter = 0
-    let newNums = nums.sort((a, b) => a - b)
-    let current = newNums.length - 1
-    let right = current - 1
-    let left = 0
-
-    console.log(newNums)
-    while (1) {
-        if (current === 2 && right === 1 && left === 0) {
-            if (newNums[current] >= newNums[right] + newNums[left]) {
-                return perimeter
-            }
-            else {
-                return newNums[current] + newNums[right] + newNums[left]
-            }
-
-        }
-        if (newNums[current] >= newNums[right] + newNums[left]) {
-            current--
-            right = current - 1
-            left = right - 1
-
-        }
-        if(newNums[current] < newNums[right] + newNums[left] ) {
-            perimeter = newNums[current] + newNums[right] + newNums[left]
-            return perimeter
-        }
+var largestSumAfterKNegations = function (nums, k) {
+    const sumArr = (arr) => {
+        return arr.reduce((total, current) => total + current, 0)
     }
-    return perimeter
+    let newNums = nums.sort((a, b) => a - b)
+
+    const indexOfZero = newNums.indexOf(0)
+
+    if (newNums[0] >= 0 && k % 2 === 0) {
+        return sumArr(newNums)
+    }
+    else if (newNums[0] >= 0 && k % 2 === 1) {
+        newNums[0] *= -1
+        return sumArr(newNums)
+    }
+    else if (newNums[0] < 0 && indexOfZero > 0) {
+        let index = 0
+        while (index < k && index < indexOfZero) {
+            newNums[index] *= -1
+            index++
+        }
+        return sumArr(newNums)
+    }
+
+    else {
+        for (let i = 0; i < newNums.length; i++) {
+            if (newNums[i] < 0) {
+                newNums[i] *= -1
+                k--
+            }
+            if (k === 0) {
+                return sumArr(newNums)
+            }
+            if (newNums[i + 1] > 0) {
+                if (k % 2 == 0) {
+                    return sumArr(newNums)
+                }
+                else {
+
+                    if (Math.abs(newNums[i]) < Math.abs(newNums[i + 1])) {
+
+
+                        newNums[i] *= -1
+                        return sumArr(newNums)
+                    }
+                    else {
+
+                        newNums[i + 1] *= -1
+                        return sumArr(newNums)
+                    }
+                }
+            }
+        }
+
+        if (k > 0 && k % 2 === 0) {
+            return sumArr(newNums)
+        }
+        else {
+            newNums[length - 1] *= -1
+            return sumArr(newNums)
+        }
+
+    }
 };
 
-console.log(largestPerimeter(nums))
+console.log(largestSumAfterKNegations(nums, k))
+
+
