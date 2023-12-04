ab0f784b0550c1fa4df2b593df7aa02ec15437ee
BuiVoQuangVinh <buivoquangvinh9@gmail.com>
2023-12-04 14:00:59 +0700

    804. Unique Morse Code Words

--- main.js
+++ main.js
@@ -1,72 +1,22 @@
-// 1005. Maximize Sum Of Array After K Negations
+// 804. Unique Morse Code Words
 
-nums = [-4, -2, -3], k = 4
+words = ["gin", "zen", "gig", "msg"]
+var uniqueMorseRepresentations = function (words) {
+    const result = new Set()
+    const hashTable = { 'a': ".-", 'b': "-...", 'c': "-.-.", 'd': "-..", 'e': ".", 'f': "..-.", 'g': "--.", 'h': "....", 'i': "..", 'j': ".---", 'k': "-.-", 'l': ".-..", 'm': "--", 'n': "-.", 'o': "---", 'p': ".--.", 'q': "--.-", 'r': ".-.", 's': "...", 't': "-", 'u': "..-", 'v': "...-", 'w': ".--", 'x': "-..-", 'y': "-.--", 'z': "--.." }
 
-var largestSumAfterKNegations = function (nums, k) {
-    const sumArr = (arr) => {
-        return arr.reduce((total, current) => total + current, 0)
-    }
-    let newNums = nums.sort((a, b) => a - b)
+    for (let i = 0; i < words.length; i++) {
+        let str = ''
+        for (let j = 0; j < words[i].length; j++) {
 
-    const indexOfZero = newNums.indexOf(0)
-
-    if (newNums[0] >= 0 && k % 2 === 0) {
-        return sumArr(newNums)
-    }
-    else if (newNums[0] >= 0 && k % 2 === 1) {
-        newNums[0] *= -1
-        return sumArr(newNums)
-    }
-    else if (newNums[0] < 0 && indexOfZero > 0) {
-        let index = 0
-        while (index < k && index < indexOfZero) {
-            newNums[index] *= -1
-            index++
+            str += hashTable[words[i][j]]
         }
-        return sumArr(newNums)
+        result.add(str)
     }
 
-    else {
-        for (let i = 0; i < newNums.length; i++) {
-            if (newNums[i] < 0) {
-                newNums[i] *= -1
-                k--
-            }
-            if (k === 0) {
-                return sumArr(newNums)
-            }
-            if (newNums[i + 1] > 0) {
-                if (k % 2 == 0) {
-                    return sumArr(newNums)
-                }
-                else {
-
-                    if (Math.abs(newNums[i]) < Math.abs(newNums[i + 1])) {
-
-
-                        newNums[i] *= -1
-                        return sumArr(newNums)
-                    }
-                    else {
-
-                        newNums[i + 1] *= -1
-                        return sumArr(newNums)
-                    }
-                }
-            }
-        }
-
-        if (k > 0 && k % 2 === 0) {
-            return sumArr(newNums)
-        }
-        else {
-            newNums[length - 1] *= -1
-            return sumArr(newNums)
-        }
-
-    }
+    return result.size
 };
 
-console.log(largestSumAfterKNegations(nums, k))
 
+console.log(uniqueMorseRepresentations(words))
 
