b648948f614baccafe7246275674af93a5c26ce3
BuiVoQuangVinh <buivoquangvinh9@gmail.com>
2023-12-06 10:15:57 +0700

    1752. Check if Array Is Sorted and Rotated

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
-// 1876. Substrings of Size Three with Distinct Characters
+// 1752. Check if Array Is Sorted and Rotated
 
 
-s = "aababcabc"
 
-var countGoodSubstrings = function(s) {
-    let numberOfGoodSubStrings = 0
-    for(let i = 0;i<s.length -2;i++){
-        let temp =   s.slice(i,i+3) 
-        if( temp[0] !== temp[1] && temp[0] !== temp[2] && temp[1] !== temp[2] ){
-            numberOfGoodSubStrings ++
+nums = [2,1,3,4]
+
+var check = function (nums) {
+    let nonDecreasing = 0
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] > nums[i + 1]) {
+            nonDecreasing++
+            if (nonDecreasing >= 2) {
+                return false
+            }
         }
-
     }
-    return numberOfGoodSubStrings
+    nums[nums.length - 1] > nums[0] && nonDecreasing++
+    return nonDecreasing < 2
 };
 
+console.log(check(nums))
 
-console.log(countGoodSubstrings(s))
