ee0f15d00411cbe70e4253220e32e83875881912
BuiVoQuangVinh <buivoquangvinh9@gmail.com>
2023-12-06 10:42:23 +0700

    1790. Check if One String Swap Can Make Strings Equal

--- main.js
+++ main.js
@@ -1,22 +1,70 @@
-// 1752. Check if Array Is Sorted and Rotated
+// 1790. Check if One String Swap Can Make Strings Equal
+
+s1 = "bank", s2 = "kanb"
 
 
 
-nums = [2,1,3,4]
+var areAlmostEqual = function (s1, s2) {
 
-var check = function (nums) {
-    let nonDecreasing = 0
-    for (let i = 0; i < nums.length - 1; i++) {
-        if (nums[i] > nums[i + 1]) {
-            nonDecreasing++
-            if (nonDecreasing >= 2) {
-                return false
-            }
+    if (s1 === s2) {
+        return true
+    }
+    let collect = []
+
+    for (let i = 0; i < s1.length; i++) {
+        if (s1[i] !== s2[i]) {
+
+            collect.push(i)
         }
     }
-    nums[nums.length - 1] > nums[0] && nonDecreasing++
-    return nonDecreasing < 2
+    console.log(collect)
+    if (collect.length !== 2) {
+        return false
+    }
+
+    else if (s1[collect[0]] === s2[collect[1]] && s2[collect[0]] === s1[collect[1]]) {
+        return true
+    }
+
+    else {
+        return false
+    }
+
+    // if( s1 === s2){
+    //     return true
+    // }
+
+    // let count = 0
+    // const hashTableS1 = {}
+    // for (let i = 0; i < s1.length; i++) {
+    //     if (!hashTableS1[s1[i]]) {
+    //         hashTableS1[s1[i]] = 1
+    //     }
+    //     else {
+    //         hashTableS1[s1[i]] += 1
+    //     }
+    // }
+
+
+    // for(let i =0;i<s1.length;i++){
+    //     if (!hashTableS1[s2[i]]){
+    //         return false
+    //     }
+    //     else{
+    //         hashTableS1[s2[i]] --
+    //         if( !hashTableS1[s2[i]] <0){
+    //             return false
+    //         }
+    //         if( s1[i] !== s2[i]){
+    //             count++
+    //             if(count >2){
+    //                 return false
+    //             }
+    //         }
+    //     }
+
+    // }
+    // return true
 };
 
-console.log(check(nums))
-
+console.log(areAlmostEqual(s1, s2))
