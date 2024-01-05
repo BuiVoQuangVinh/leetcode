7173b0585a2056a6601c4c94e8874efebb9cbb56
BuiVoQuangVinh <buivoquangvinh9@gmail.com>
2024-01-05 11:06:19 +0700

    change code

--- main.js
+++ main.js
@@ -1,35 +1,30 @@
 // 504. Base 7
 
 var convertToBase7 = function (num) {
-  const result = []
-  let numberBase7 = 0
+  let result = ""
+
   if (num == 0) {
     return '0'
   }
   else if (num > 0) {
     while (num > 0) {
       const temp = num % 7;
-      result.push(temp)
+      result =temp+result;
       num = (num - temp) / 7;
     }
-    while (result.length > 0) {
-      numberBase7 = numberBase7 * 10 + result.pop();
-    }
-    return `${numberBase7}`;
+
+    return result;
   }
   else {
     num *= -1
     while (num > 0) {
       const temp = num % 7;
-      result.push(temp)
+      result = temp+result;
       num = (num - temp) / 7;
     }
-    while (result.length > 0) {
-      numberBase7 = numberBase7 * 10 + result.pop();
-    }
-    return `${numberBase7*(-1)}`;
+    return `-${result}`;
   }
 };
 
-num = -565
+num = 565
 console.log(convertToBase7(num))
